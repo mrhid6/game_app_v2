f64e5b28459c0d41fbc62c8737a1ee7bba42c1a9
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2016-11-26 22:58:16 +0000

    added reference config file

---
+++ config.exmaple.js
@@ -0,0 +1,15 @@
+
+var Config = {
+    mysql: {
+        connections: 100,
+        host: "",
+        user: "",
+        password: "",
+        database: "",
+    },
+    general:{
+        timesyncInterval: 60000
+    }
+};
+
+module.exports = Config;
